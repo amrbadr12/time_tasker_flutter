5ea97b5b6a4873a9745c3df3a1e3865151d52062
amrbadr12 <amr.badr.9620@gmail.com>
2020-08-30 21:47:04 +0200

    -Responsive UI from add_task_reusable_cards.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:time_tasker/constants.dart';
@@ -10,10 +11,10 @@ import 'add_task_screens/add_task_screen.dart';
 
 void main() {
   runApp(
-//    DevicePreview(
-//      builder: (context) => MyApp(),
-//    ),
-    MyApp(),
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+    // MyApp(),
   );
 }
 
